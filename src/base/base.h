744c7a79db3056115f0591ce1049cabd8870a90b
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-09-16 12:20:39 +0200

    base base.h

---
+++ src/base/base.h
@@ -0,0 +1,46 @@
+#ifndef BASE
+#define BASE
+
+// basic types
+#include <stdint.h>
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+typedef uint8_t U8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef float f32;
+typedef double f64;
+
+#define AssertBreak()                                                          \
+    {                                                                          \
+        char *c = NULL;                                                        \
+        *c = 0;                                                                \
+    }
+
+#define Assert(e)                                                              \
+    if (!(e)) {                                                                \
+        printf("%s failed\n", #e);                                             \
+        AssertBreak();                                                         \
+    }
+
+#define ArrayCount(a) (sizeof(a) / sizeof(*(a)))
+
+#define IntFromPtr(p) (unsigned long long)((char *)p - (char *)0)
+
+#define Member(T, m) (((T *)0)->m)
+#define OffsetOfMember(T, m) IntFromPtr(&Member(T, m))
+
+#define Min(a, b) (((a) < (b)) ? (a) : (b))
+#define Max(a, b) (((a) > (b)) ? (a) : (b))
+
+// debug printing
+#define EvalPrintInt(x) printf("%s = %d\n", #x, x)
+#define EvalPrintUInt(x) printf("%s = %u\n", #x, x)
+#define EvalPrintFloat(x) printf("%s = %f\n", #x, x)
+#define EvalPrintPtr(x) printf("%s = %p\n", #x, x)
+#define EvalPrintChar(x) printf("%s = '%c'\n", #x, x)
+#define EvalPrintString(x) printf("%s = \"%s\"\n", #x, x)
+
+#endif // !BASE
--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
+#include "base/base.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
+    char str[] = "Hello World";
+    Assert(ArrayCount(str) == 12);
+    Assert(Min(1, 2) == 1);
+    Assert(Max(1, 2) == 2);
+
     printf("Hello World!\n");
 
     return EXIT_SUCCESS;
