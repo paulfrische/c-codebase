eb898aaa934d8b873b20882cd5fa023e5c6ef45e
Paul Frische <61984114+paulfrische@users.noreply.github.com>
2023-09-16 17:43:56 +0200

    rename some types

--- src/base/base.h
+++ src/base/base.h
@@ -3,10 +3,11 @@
 
 // basic types
 #include <stdint.h>
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
-typedef uint8_t U8;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
